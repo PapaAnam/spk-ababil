63396a03da6a2a361a2a0ea87b23cef5c25f5683
PapaAnam <hairulanam21@outlook.com>
2018-07-07 19:01:50 +0700

    gaji

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/', function(){
+	return redirect()->route('satuan');
+});
+
 Route::prefix('satuan')->group(function(){
 	Route::get('/', 'SatuanController@index')->name('satuan');
 	Route::get('/tambah', 'SatuanController@create')->name('satuan.tambah');
